ddeb7cba17ac56d672e097b37e730e55a24e4053
madhav vengalil <madhav1709@gmail.com>
2018-04-03 09:05:11 +0530

    Update Cart.js

--- app/models/Cart.js
+++ app/models/Cart.js
@@ -3,14 +3,14 @@ var Schema = mongoose.Schema;//declaring a schema
 
 var cartSchema = new Schema({
 
-    p_id 			: {type:Number},
-	p_name  		: {type:String,default:''},
+        p_id 		    : {type:Number},
+	p_name              : {type:String,default:''},
 	p_color  	    : {type:String,default:''},
 	p_size	  	    : {type:String,default:''},
 	p_cost	  	    : {type:String,default:''},
-	p_category	  	: {type:String,default:''},
-	username	    : {type:String,require:true},
-	userEmail       : {type:String,require:true}
+	p_category	    : {type:String,default:''},
+	username	    : {type:String,require:true}
+	
 	
 });//cart model
 
